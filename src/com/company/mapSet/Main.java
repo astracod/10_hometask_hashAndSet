4eaf27375875bd30ecd4f8749f1106f965002b41
Zhulikov_Dmitriy <titan_22@mail.ru>
2020-10-29 00:38:33 +0300

    изменения

--- src/com/company/mapSet/Main.java
+++ src/com/company/mapSet/Main.java
@@ -1,54 +1,26 @@
 package com.company.mapSet;
 
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
         List<String> names = new ArrayList<String>();
-        Collections.addAll(names, "Дмитрий", "Дмитрий", "Дмитрий", "Артур", "Артур", "Лев", "Илья", "Василий", "Оксана", "Оксана", "Вел");
+        Collections.addAll(names, "Илья", "Дмитрий", "Артур", "Артур", "Лев", "Дмитрий", "Василий", "Оксана", "Оксана", "Вел", "Дмитрий");
 
         HashMap<String, Integer> mapNames = new HashMap<>();
         HashMap<String, Integer> mapNames2 = new HashMap<>();
         HashSet<String> setNames = new HashSet<>();
 
-   /*     for (int i = 0; i < names.size(); i++) {
-
-            String targetNames = names.get(i);
-            for (int j = i; j < names.size(); j++) {
-                if (targetNames.equals(names.get(j))) {
-                    quantity++;
-                    if (i != names.size() - 1 && (targetNames.equals(names.get(j + 1)))) {
-                        i = i + 1;
-                    }
-                }
-            }
-
-            for (int j = 0; j < names.size(); j++) {
-                mapNames.put(targetNames, quantity);
-                if (mapNames.containsValue(1)) {
-                    mapNames.remove(targetNames);
-                    setNames.add(targetNames);
-                }
-            }
-        }*/
-        int quantity = 1;
         for (String name : names) {
-            if (mapNames.containsKey(name)) {
-                quantity++;
-                mapNames.put(name, quantity);
-            }
-            if (!(mapNames.containsKey(name))){
-                quantity = 1;
-                mapNames.put(name, quantity);
-            }
-
+            if (mapNames.containsKey(name)) mapNames.put(name, mapNames.get(name) + 1);
+            if (!(mapNames.containsKey(name))) mapNames.put(name, 1);
         }
 
-        for ( Map.Entry<String,Integer> item : mapNames.entrySet()) {
-            if (item.getValue() == 1)  setNames.add(item.getKey());
+        for (Map.Entry<String, Integer> item : mapNames.entrySet()) {
+            if (item.getValue() == 1) setNames.add(item.getKey());
             if (item.getValue() >= 2) mapNames2.put(item.getKey(), item.getValue());
         }
 
